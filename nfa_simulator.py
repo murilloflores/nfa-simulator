4129346c8431303b209e1370d9d55699a0fda0be
Murillo Flores <murillo.lf@gmail.com>
2016-10-06 17:25:07 -0300

    refactoring

--- nfa_simulator.py
+++ nfa_simulator.py
@@ -7,38 +7,44 @@ class NondeterministicFiniteAutomata(object):
 		self.definition = definition
 
 	def compute(self, string):
-		return self.accept(self.definition['q0'], string)
-
-	def epsilon_transition(self, state):
-		return self.definition['delta'].get(state, dict()).get('epsilon', [])
-
-	def transition(self, state, char):
-		next_states = self.definition['delta'].get(state, dict()).get(char, [])
-		epsilon_next_states = []
-		for next_state in next_states:
-			epsilon_next_states.extend(self.epsilon_transition(next_state))
-
-		next_states.extend(epsilon_next_states)
-		return next_states
+		return self.accept(self.start_state(), string)
 
 	def accept(self, state, string):
 		if(len(string) == 0):
-			return state in self.definition['f']
+			return self.is_final_state(state)
 
 		char = string[0]
-		next_states = self.transition(state, char)
-		for next_state in next_states:
-			if self.accept(next_state, string[1:]):
+		remaining_string = string[1:]
+
+		for next_state in self.possible_next_states(state, char):
+			if self.accept(next_state, remaining_string):
 				return True
 
 		return False
 
-def parse_definition(definition_file_path):
-	return json.loads(open(definition_file_path, 'r').read())
+	def possible_next_states(self, state, char):
+		next_states = []
+		
+		for next_state in self.transition(state, char):
+			next_states.append(next_state)
+			next_states.extend(self.transition(next_state, 'epsilon'))
+
+		return next_states
+
+	# Auxiliar methods
+
+	def start_state(self):
+		return self.definition['q0']
+
+	def is_final_state(self, state):
+		return state in self.definition['f']
+
+	def transition(self, state, char):
+		return self.definition['delta'].get(state, dict()).get(char, [])
 
 if __name__ == '__main__':
-	nfa_definition = parse_definition(sys.argv[1])
+	definition = json.loads(open(sys.argv[1], 'r').read())
 	string = sys.argv[2]
 
-	nfa = NondeterministicFiniteAutomata(nfa_definition)
+	nfa = NondeterministicFiniteAutomata(definition)
 	print(nfa.compute(string))
